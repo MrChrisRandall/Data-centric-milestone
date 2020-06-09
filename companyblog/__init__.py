4bf3c993057649fcaa3669013b54bccc292bace2
MrChrisRandall <chris7890@live.ie>
2020-06-09 16:58:09 +0000

    Blog post forms, views + templates complete

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 
 from companyblog.error_pages.handlers import error_pages
 from companyblog.users.views import users
+from companyblog.blog_posts.views import blog_posts
 from companyblog.core.views import core
 
 
@@ -40,4 +41,5 @@ login_manager.login_view = 'users.login'
 
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
